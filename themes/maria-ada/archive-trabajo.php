8eee828d18a0bb87dfad2e5cee7c3ff1a44e3a32
Brais Cao <braiscao.dev@gmail.com>
2018-11-21 13:36:41 +0100

    Añadido wrapper a cada item del portfolio

--- themes/maria-ada/archive-trabajo.php
+++ themes/maria-ada/archive-trabajo.php
@@ -9,13 +9,15 @@
         $imageID = attachment_url_to_postid($imageURL);
         ?>
 
-        <div class="portfolio-item">
-            <a href="<?php echo get_permalink() ?>" class="portfolio-item__link">
-                <span class="portfolio-item__title">
-                    <?php the_title() ?>
-                </span>
-                <img class="portfolio-item__image" src="<?php echo esc_attr(wp_get_attachment_image_src($imageID, 'portafolio_miniatura')[0]);?>" alt="">
-            </a>
+        <div class="portfolio-item-wrapper">
+            <div class="portfolio-item">
+                <a href="<?php echo get_permalink() ?>" class="portfolio-item__link">
+                    <span class="portfolio-item__title">
+                        <?php the_title() ?>
+                    </span>
+                    <img class="portfolio-item__image" src="<?php echo esc_attr(wp_get_attachment_image_src($imageID, 'portafolio_miniatura')[0]);?>" alt="">
+                </a>
+            </div>
         </div>
 
 
