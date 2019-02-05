ca1e8ac1e3771ae1b20b5456fc5f43c97e5ddbcb
Brais Cao <braiscao.dev@gmail.com>
2019-02-05 01:17:59 +0100

    Sketchbook page margins

--- themes/maria-ada/page-sketchbook.php
+++ themes/maria-ada/page-sketchbook.php
@@ -16,32 +16,13 @@
             <?php the_title() ?>&nbsp;
         </h2>
         <div class="sketchbook__slider">
-            <ul id="lightSlider">
-                <?php 
-            
-        foreach( $gallery as $image_url ) {
-            $originalImageUrl = substr($image_url, 0, -12) . '.jpg';//Conseguir la URL de la imagen original a partir de la thumbnail 'str_replace('-150x150', '', $image_url);'
-            $imageID = attachment_url_to_postid($originalImageUrl); //Conseguir la Id a partir de la URL
-            $imageURL = wp_get_attachment_image_src($imageID, 'fullhd')[0]; //Conseguir la URL en tamaño portafolio (1080p)
-            $img_srcset = wp_get_attachment_image_srcset( $imageID, 'full' );
-            ?>
-
-                <li class="sketchbook__slide">
-                    <img src="<?php echo esc_attr($imageURL)?>" class="sketchbook__image" srcset="<?php echo esc_attr( $img_srcset );?>"
-                        sizes="60vw" alt="">
-                </li>
-                <?php } 
+            <?php 
+            the_content(); 
         }
         ?>
             </ul>
         </div>
     </section>
 </div>
-<script type="text/javascript">
-    jQuery(function ($) {
-        $(document).ready(function () {
-            $("#lightSlider").lightSlider();
-        });
-    });
-</script>
+
 <?php get_footer() ?>
