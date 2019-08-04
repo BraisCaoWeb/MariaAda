91e361aafeb82a307028ae19519bc7b10ef3a0c9
Brais Cao <braiscao.dev@gmail.com>
2019-08-04 14:45:53 +0200

    Cambios

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,48 @@
-const path = require("path");
-const settings = require("./settings");
-const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const path = require('path');
+const settings = require('./settings');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
-    entry: [
-        "babel-polyfill",
-        settings.themeLocation + "js/main.js",
-        settings.themeLocation + "sass/main.scss"
-    ],
+    entry: ['babel-polyfill', settings.themeLocation + 'js/main.js', settings.themeLocation + 'sass/main.scss'],
     output: {
-        path: path.resolve(__dirname, settings.themeLocation + "js"),
-        filename: "main.min.js"
+        path: path.resolve(__dirname, settings.themeLocation + 'js'),
+        filename: 'main.min.js',
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "babel-loader"
+                loader: 'babel-loader',
             },
             {
                 test: /\.scss$/,
                 use: [
                     {
-                        loader: "file-loader",
+                        loader: 'file-loader',
                         options: {
-                            name: "style.css",
-                            outputPath: "../"
-                        }
+                            name: 'style.css',
+                            outputPath: '../',
+                        },
                     },
                     {
-                        loader: "extract-loader"
+                        loader: 'extract-loader',
                     },
                     {
-                        loader: "css-loader"
+                        loader: 'css-loader',
                     },
                     {
-                        loader: "sass-loader"
-                    }
-                ]
-            }
-        ]
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
+        ],
     },
     plugins: [
         new BrowserSyncPlugin({
             proxy: settings.urlToPreview,
-            files: ["**/*.php", "*.php", "**/*.css", "**/*.js"],
-            reloadDelay: 0
-        })
-    ]
+            files: ['**/*.php', '*.php', '**/*.css', '**/*.js'],
+            reloadDelay: 0,
+        }),
+    ],
 };
